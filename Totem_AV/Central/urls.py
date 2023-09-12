1ab34135635507a357b8f5997086a64d13c71569
DiegoRiosGo <die.rios@duocuc.cl>
2023-09-12 16:07:29 -0300

    variables

--- Totem_AV/Central/urls.py
+++ Totem_AV/Central/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Principal
+from .views import Principal,Totem
 
 urlpatterns = [
     #html cliente
     path('',Principal,name="Principal"),
-
+    path('Totem/',Totem,name="Totem"),
 
 ]
--- Totem_AV/Central/views.py
+++ Totem_AV/Central/views.py
@@ -1,5 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def Principal(request):
-    return render(request,'Central/HTML/Principal.html')
+    
+        # Retrieve data for other form fields similarly
+
+        # You can process the data here if needed
+    if request.method == 'POST':
+        # Retrieve form data from POST request
+        sede = request.POST.get('Sede')
+        boton1_nombre = request.POST.get('nombreBoton1')
+        boton1_url = request.POST.get('urlBoton1')
+        # Render the result page
+       
+        return render(request, 'Central/HTML/Principal.html',{
+            'sede': sede,
+            'boton1_nombre': boton1_nombre,
+            'boton1_url': boton1_url,
+            # Add other data to the context
+        })
+    
+    
+
+def Totem(request):
+    
+    return render(request,'Central/HTML/Totem.html')
